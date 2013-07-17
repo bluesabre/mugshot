cb14af6893a4648c25f164f6d066022707b40a59
Sean Davis <smd.seandavis@gmail.com>
2013-07-17 13:17:16 -0400

    Use icon name instead of path

--- setup.py
+++ setup.py
@@ -91,9 +91,7 @@ def update_desktop_file(filename, target_pkgdata, target_scripts):
         fout = file(filename + '.new', 'w')
 
         for line in fin:
-            if 'Icon=' in line:
-                line = "Icon=%s\n" % (target_pkgdata + 'media/mugshot.svg')
-            elif 'Exec=' in line:
+            if 'Exec=' in line:
                 cmd = line.split("=")[1].split(None, 1)
                 line = "Exec=%s" % (target_scripts + 'mugshot')
                 if len(cmd) > 1:
