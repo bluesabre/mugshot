83544bda793493f792f1c3263bd85a05dbeac190
Sean Davis <smd.seandavis@gmail.com>
2014-03-02 10:50:40 -0500

    Cleanup license, PEP8 goodness

--- mugshot/__init__.py
+++ mugshot/__init__.py
@@ -1,28 +1,30 @@
+#!/usr/bin/python3
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
-### BEGIN LICENSE
-# Copyright (C) 2013 Sean Davis <smd.seandavis@gmail.com>
-# This program is free software: you can redistribute it and/or modify it 
-# under the terms of the GNU General Public License version 3, as published 
-# by the Free Software Foundation.
-# 
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranties of 
-# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
-# PURPOSE.  See the GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License along 
-# with this program.  If not, see <http://www.gnu.org/licenses/>.
-### END LICENSE
+#   Mugshot - Lightweight user configuration utility
+#   Copyright (C) 2013-2014 Sean Davis <smd.seandavis@gmail.com>
+#
+#   This program is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License version 3, as published
+#   by the Free Software Foundation.
+#
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranties of
+#   MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import optparse
 
 from locale import gettext as _
 
-from gi.repository import Gtk # pylint: disable=E0611
+from gi.repository import Gtk  # pylint: disable=E0611
 
 from mugshot import MugshotWindow
 
 from mugshot_lib import set_up_logging, get_version
+
 
 def parse_options():
     """Support for command line options"""
@@ -34,11 +36,12 @@ def parse_options():
 
     set_up_logging(options)
 
+
 def main():
     'constructor for your class instances'
     parse_options()
 
-    # Run the application.    
+    # Run the application.
     window = MugshotWindow.MugshotWindow()
     window.show()
     Gtk.main()
